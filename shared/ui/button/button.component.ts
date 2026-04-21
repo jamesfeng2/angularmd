e171c200f3f54e1a8a181785eead21c548b04cf1
jamesfeng2 <jamesfeng2@users.noreply.github.com>
2026-04-21 21:21:41 +0000

    a

--- shared/ui/button/button.component.ts
+++ shared/ui/button/button.component.ts
@@ -5,11 +5,11 @@ import { ButtonVariant, ButtonSize } from './button.types';
   selector: 'app-button',
   template: `
     <button
-      class="app-button" // 这个 class 是普通 HTML 永远存在，不需要 Angular 参与 <button class="app-button">
-      [class.full-width]="fullWidth" // 动态绑定 由 Angular 根据表达式决定
+      class="app-button" // 永远存在 这个 class 是普通 HTML 永远存在，不需要 Angular 参与 <button class="app-button">
+      [class.full-width]="fullWidth" //是否存在 动态绑定 由 Angular 根据表达式决定 class 是否存在
       [class.loading]="loading"
       [disabled]="disabled || loading" // 当 disabled 为 true，或者 loading 为 true 时，这个按钮就会被禁用。
-      [ngClass]="[variant, size]"
+      [ngClass]="[variant, size]"  //批量添加
       (click)="clicked.emit()"
     >
       <span class="spinner" *ngIf="loading"></span>
